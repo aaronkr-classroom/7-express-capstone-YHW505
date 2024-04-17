fc14a4f8d06844bc290eae67602498560b9b664f
YHW505 <126962832+YHW505@users.noreply.github.com>
2024-04-17 02:58:19 +0000

    first commit (project not done)

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -18,3 +18,36 @@
  * [노트] 라우트 순서는 중요하다. 이 라우트는 기존에 존재하는 라우트 아래에 와야 한다.
  * 기존 라우트는 범용으로 사용되는 것이며 하위 라우트에 오버라이딩되기 때문이다.
  */
+
+const httpStatus = require("http-status-codes");
+
+/**
+ * Listing 11.2 (p. 168)
+ * 에러 컨트롤러 추가
+ */
+exports.logErrors = (err, req, res, next) => {
+    console.error(err.stack);
+    next(err);
+}; // @TODO: 에러 처리를 위한 미들웨어 추가
+
+/**
+ * Listing 11.3 (p. 169)
+ * 사용자 정의 메시지로 빠진 라우트 및 에러 대응
+ */
+exports.resNotFound = (req, res) => {
+    let errorCode = httpStatus.NOT_FOUND;
+    res.status(errorCode);
+    //res.send('~${errorCode} | The resource does not exist!`);
+    res.sendFile(`./public/${errorCode}.html`, {
+        root:'./'
+    });
+}; // @TODO: 404 상태 코드로 모든 에러 처리. 404.html 파일의 콘텐츠 전송
+exports.resInternalError = (err, req, res, next) => {
+    let errorCode = httpStatus.INTERNAL_SERVER_ERROR; // 500
+    console.error(`ERROR ${err.stack}`);
+    res.status(errorCode);
+    //res.send('~${errorCode} | The resource does not exist!`);
+    res.sendFile(`./public/${errorCode}.html`, {
+        root:'./'
+    });
+}; // @TODO: 500 상태 코드로 모든 에러 처리
--- main.js
+++ main.js
@@ -2,30 +2,72 @@
 // Capstone 2: Express
 "use strict";
 
+//const { application } = require("express");
+const express = require("express"),
+    homeController = require('./controllers/homeController'),
+    errorController = require('./controllers/errorController'),
+    app = express();
+app.set("port", process.env.PORT || 3001);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Confetti Cuisine!");
+})
+
+
 // 앱 설정
 
 /**
  * Listing 12.7 (p. 179)
  * ejs 레이아웃 렌더링
  */
-
+const layouts = require("express-ejs-layouts");
+app.set("view engine", "ejs");
+app.use(layouts);
+app.use(express.static("public"));
 
 /**
  * Listing 12.4 (p. 177)
  * body-parser의 추가
  */
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+app.use(express.json());
 
 
 /**
  * Listing 12.6 (p. 178)
  * 각 페이지 및 요청 타입을 위한 라우트 추가
  */
+app.get("/courses", homeController.showCourses);
+app.get("/contact", homeController.showSignUp);
+app.get("/contact", homeController.postedSignUpForm);
 
 
 /**
  * Listing 12.12 (p. 184)
  * 에러 처리 라우트 
  */
+const httpStatus = require('http-status-codes');
 
+exports.PageNotFoundError = (req, res) => {
+    let errorCode = httpStatus.NOT_FOUND;
+    res.status(errorCode);
+    res.render("error");
+}
+exports.internalServerError = (err, req, res, next) => {
+    let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
+    console.log(`ERROR occurred: ${error.stack}`);
+    res.render(errorCode);
+    res.send(`${errorCode} | Sorry, our application is taking a nap!`);
+}
 
 // 3000번 포트로 리스닝 설정
+app.listen(app.get("port"), () => {
+    console.log(`Server runngin at http://localhost:${app.get(
+        "port"
+    )}`
+);
+});
